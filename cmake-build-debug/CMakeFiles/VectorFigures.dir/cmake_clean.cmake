ec00b64cf1c1102159a2d38dd2867af8b2ba2a9a
menegais1 <menegais1@gmail.com>
2020-04-19 00:51:24 -0300

    Adding a cmake project, started using clion for development

---
+++ cmake-build-debug/CMakeFiles/VectorFigures.dir/cmake_clean.cmake
@@ -0,0 +1,28 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/VectorFigures.dir/src/Base/CanvasObject.cpp.o"
+  "CMakeFiles/VectorFigures.dir/src/Bounds.cpp.o"
+  "CMakeFiles/VectorFigures.dir/src/Button/Button.cpp.o"
+  "CMakeFiles/VectorFigures.dir/src/Canvas/gl_canvas2d.cpp.o"
+  "CMakeFiles/VectorFigures.dir/src/ColorPicker/ColorPicker.cpp.o"
+  "CMakeFiles/VectorFigures.dir/src/ColorPicker/ColorPickerPanel.cpp.o"
+  "CMakeFiles/VectorFigures.dir/src/ColorPicker/ColorSlider.cpp.o"
+  "CMakeFiles/VectorFigures.dir/src/Figure.cpp.o"
+  "CMakeFiles/VectorFigures.dir/src/FigureListManager.cpp.o"
+  "CMakeFiles/VectorFigures.dir/src/Label/Label.cpp.o"
+  "CMakeFiles/VectorFigures.dir/src/Managers/GlobalManager.cpp.o"
+  "CMakeFiles/VectorFigures.dir/src/Panel/Panel.cpp.o"
+  "CMakeFiles/VectorFigures.dir/src/Scene.cpp.o"
+  "CMakeFiles/VectorFigures.dir/src/Slider/Slider.cpp.o"
+  "CMakeFiles/VectorFigures.dir/src/Utilities.cpp.o"
+  "CMakeFiles/VectorFigures.dir/src/Vectors/Float2.cpp.o"
+  "CMakeFiles/VectorFigures.dir/src/Vectors/Float3.cpp.o"
+  "CMakeFiles/VectorFigures.dir/src/Vectors/Float4.cpp.o"
+  "CMakeFiles/VectorFigures.dir/src/main.cpp.o"
+  "VectorFigures"
+  "VectorFigures.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/VectorFigures.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
